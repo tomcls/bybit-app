8a2a95f6800619bc3a1d9ccf3198779d85a9595b
Thomas Claessens <tom@itcl.io>
2024-03-17 15:27:17 +0100

    ...

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,8 +3,10 @@ import 'dotenv/config'
 export const ConfigParams:any = {
     BYBIT_TEST1_API_KEY: process.env.BYBIT_TEST1_API_KEY || "",
     BYBIT_TEST1_API_SECRET: process.env.BYBIT_TEST1_API_SECRET || "",
-    BYBIT_TEST2_API_KEY: process.env.BYBIT_TEST1_API_KEY || "",
-    BYBIT_TEST2_API_SECRET: process.env.BYBIT_TEST1_API_SECRET || "",
+    BYBIT_TEST2_API_KEY: process.env.BYBIT_TEST2_API_KEY || "",
+    BYBIT_TEST2_API_SECRET: process.env.BYBIT_TEST2_API_SECRET || "",
+    BYBIT_TEST3_API_KEY: process.env.BYBIT_TEST3_API_KEY || "",
+    BYBIT_TEST3_API_SECRET: process.env.BYBIT_TEST3_API_SECRET || "",
     TEST_NET: true,
     URL: process.env.BYBIT_TESTNET_URL || ""
 }
